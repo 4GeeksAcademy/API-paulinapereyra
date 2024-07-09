4e814c7afd4e8a759b2f8a08d0a37916993377bf
Paulina-Pereyra <paulinapereyrasantana@gmail.com>
2024-07-09 20:49:05 +0000

    proyecto API terminado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,20 +1,15 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const Home = () => {
-	const [tarea, setTarea] = useState("");
-	const [lista, setLista] = useState([
-		{ id: 1, texto: "Comer", completada: false },
-		{ id: 2, texto: "Lavarse las manos", completada: false },
-		{ id: 3, texto: "Hacer la cama", completada: false },
-		{ id: 4, texto: "Pasear al perro", completada: false }
-	]);
+	let [tarea, setTarea] = useState("");
+	let [lista, setLista] = useState([])
 
 	const agregarTarea = () => {
 		if (tarea.trim() !== "") {
 			const nuevaTarea = {
-				id: lista.length + 1,
-				texto: tarea,
-				completada: false
+				// id: lista.length + 1,
+				label: tarea,
+				is_done: false
 			};
 			setLista([...lista, nuevaTarea]);
 			setTarea("");
@@ -25,6 +20,34 @@ const Home = () => {
 		setLista(lista.filter((tarea) => tarea.id !== id));
 	};
 
+	function crearUsuario() {
+		fetch('https://playground.4geeks.com/todo/users/PaulinaPereyra', {
+			method:'POST',
+			headers: {
+				"Content-Type" :"application/json"
+			}
+		})
+
+	}
+	function obtenerTareas () {
+		fetch('https://playground.4geeks.com/todo/users/PaulinaPereyra', {
+			method:'GET',
+			headers: {
+				"Content-Type" :"application/json"
+			}
+	})
+
+	.then((response) => response.json())
+	.then((data) => setLista(data.todos))
+	.catch((error) => console.log(error));
+	
+	}
+	useEffect(() =>{
+			crearUsuario()
+			obtenerTareas() 
+		}, [])
+
+		
 
 	return (
 		<div className="container text-center">
@@ -38,8 +61,7 @@ const Home = () => {
 				<ul className="list-unstyled">
 					{lista.map((tarea) => (
 						<li key={tarea.id}>
-							{tarea.texto}
-							
+							{tarea.label}	
 							<button type="button" className="btn btn-light ms-2" onClick={() => eliminarTarea(tarea.id)}>x</button>
 						</li>
 					))}
